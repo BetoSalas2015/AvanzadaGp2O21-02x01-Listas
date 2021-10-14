0f3bad9b2cc3d8905b7aadc93b718a37178a3497
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2021-10-14 17:26:43 -0500

    Sesión 26 - 14/10/2021

--- Listas.c
+++ Listas.c
@@ -1,36 +1,29 @@
-//  Sesión 25 - 13/10/2021
+//  Sesión 26 - 14/10/2021
 
 #include <stdio.h>		//  par ala I/O del programa
 #include <stdlib.h>		//  para system()
 #include <malloc.h>		// "Memory ALLOCation" - Asignacion de memoria -
 
+struct Nodo 
+{
+	char info;
+	struct Nodo *sig:
+};						//  DEfinimos las características del Nodo
+
+typedef struct Nodo nodo;	//  Definimos el nuevo tipo de dato nodo
+
+nodo *raiz = NULL;			// DEfiendo una lista ligada vacía
+
+void insert(char dato)
+{
+	nodo *nuevo = NULL;
+	nuevo = (nodo *) malloc( sizeof(nodo) ); // Creamos el nuevo nodo
+}
+
 int main(void)
 {
-	int *ptra, *ptrb, *ptrc;	// Creamos tre apuntadores a entero
-
-	ptra = (int *) malloc( sizeof( int ) );		//  Administración dinámica de la memoria
-	if( ptra == NULL)
-	{
-		printf("No hay memoria suficiente\n");
-		system("pause");
-		return 1;
-	}
-	ptrb = (int *) malloc( sizeof( int ) );
-	ptrc = (int *) malloc( sizeof( int ) );
-
-	printf("Dame el valor de a: ");
-	scanf("%d", ptra);
-	printf("Dame el valor de b: ");
-	scanf("%d", ptrb);
-
-	*ptrc = *ptra + *ptrb;
-
-	printf("La suma es: %d \n", *ptrc);
-
-	free(ptra);				// Liberamos la meoria reservada por malloc()
-	free(ptrb);
-	free(ptrc);
-
+	int edad;
+	nodo primero;
 	system("pause");
 	return 1;
 }
